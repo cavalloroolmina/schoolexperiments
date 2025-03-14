158b9892bf68f0cc50e8ef393f35de3aa2bbdcdf
cavalloroolmina <cavalloroolmina@gmail.com>
2025-03-14 16:12:23 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,10 @@
+interface Product {
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { name: "Product A", price: 10 },
+  { name: "Product B", price: 20 },
+  { name: "Product C", price: 30 }
+];
